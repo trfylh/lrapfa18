ac5a24e720dc08ac9bbeaf24df4153d6290cfa86
Teela H <thuff@berkeley.edu>
2018-11-07 15:00:25 -0800

    phonemic form first version

--- panara_orth_to_phon.js
+++ panara_orth_to_phon.js
@@ -4,28 +4,26 @@
 // Returns a dict of Panãra consonant phonetic forms that change for phonemic forms
 function GETPHONCHANGED(INPUT1) {
     var cons;
-    cons = {}; // all consonants except ɲ (done after syllabification)
-    cons["p"] = "p"; // singleton obstruent
-    cons["t"] = "t";
-    cons["s"] = "s";
-    cons["k"] = "k";
-    cons["pp"] = "pp"; // geminate obstruent
-    cons["tt"] = "tt";
-    cons["ss"] = "ss";
-    cons["kk"] = "kk";
-    cons["m"] = "m"; // singleton nasal
-    cons["n"] = "n";
-    cons["j"] = "ŋ"; // ŋ<j> == ["j"] = "ŋ"
-    cons["mm"] = "mm"; // geminate nasal
-    cons["nn"] = "nn";
-    cons["mp"] = "mp"; // post-oralized nasal
-    cons["nt"] = "nt";
-    cons["ns"] = "ns";
-    cons["ŋk"] = "ŋk";
-    cons["w"] = "w"; // approximant
-    cons["r"] = "ɾ";
-    cons["j"] = "j";
+    cons = {}; // all cons that change
+    cons["mp"] = "m"; // post-oralized nasal
+    cons["nt"] = "n";
+    cons["ns"] = "ɲ";
+    cons["ŋk"] = "ŋ";
     return cons;
+}
+
+// Returns a list of nt and gemintates for checking if word initial i is epenthetic
+function GETINITIALEPEN(INPUT1) {
+    var ntgems;
+    ntgems = [];
+    ntgems.push("nt"); // short oral
+    ntgems.push("pp");
+    ntgems.push("tt");
+    ntgems.push("ss");
+    ntgems.push("kk");
+    ntgems.push("mm");
+    ntgems.push("nn");
+    return ntgems;
 }
 
 // Returns a dict of Panãra consonants orthography conventions
@@ -93,6 +91,7 @@ function GETV(INPUT1) {
     return vowels;
 }
 
+// Returns a dict of POA for C for syllabification
 function GETCPOA(INPUT1) {
     var cons;
     cons = {};
@@ -110,6 +109,7 @@ function GETCPOA(INPUT1) {
     return cons;
 }
 
+// Returns a list of V
 function GETSYLLV(INPUT1) {
     var vowels;
     vowels = [];
@@ -138,6 +138,7 @@ function GETSYLLV(INPUT1) {
     vowels.push("ã");
     vowels.push("õ");
     vowels.push("ĩ:"); // long nasal
+    vowels.push("ɯ̃:");
     vowels.push("ũ:");
     vowels.push("ẽ:");
     vowels.push("ã:");
@@ -145,6 +146,7 @@ function GETSYLLV(INPUT1) {
     return vowels;
 }
 
+// Returns a list of nasal V
 function GETNASALV(INPUT1) {
     var vowels;
     vowels = [];
@@ -162,7 +164,7 @@ function GETNASALV(INPUT1) {
     return vowels;
 }
 
-// Preceding V for n in coda that becomes ŋ {u, u:, ũ, ũ:, o, o:, õ, õ:, ɔ, ɔ:}
+// Returns a list of preceding V for n in coda that becomes ŋ {u, u:, ũ, ũ:, o, o:, õ, õ:, ɔ, ɔ:}
 function GETENGMAV(INPUT1) {
     var vowels;
     vowels = [];
@@ -204,7 +206,7 @@ function SYLLABIFYPHONETIC(INPUT1) {
             }
         }
         else if (afterV == 1) { // if in syllable coda, at potential C3
-            if (i != phon.length - 1) { // if two or more C left
+            if (i < phon.length - 1) { // if two or more C left
                 var next = phon.charAt(i + 1);
                 if (cPOA[first] == cPOA[next]) { // if POA matches, then VC3.
                     syll += first + ".";
@@ -260,7 +262,7 @@ function ORTHTOPHONETIC(INPUT1) {
     // move consider current two, if not in cons or vowels, then assign first
     // and move one forward, repeat
     for (i = 0; i < orth.length; i++) {
-        if (i != orth.length - 1) { // if two or more letters left
+        if (i < orth.length - 1) { // if two or more letters left
             var first = orth.charAt(i);
             var pair = first + orth.charAt(i + 1);
             if (pair in cons) {
@@ -297,8 +299,8 @@ function PHONFILL(INPUT1){
     var filled;
     filled = INPUT1;
     filled = PHONETICFILL(filled);
-    filled = PHONEMICFILL(filled);
     filled = NFILL(filled);
+    filled = TWOPHONFILL(filled);
     return filled;
 }
 
@@ -366,7 +368,7 @@ function PHONETICN(INPUT1) {
             // if j is first, or if j is preceded by a syllable break (making it C1V)
             if (i == 0 || (i != 0 && oldphon.charAt(i - 1) == ".")) {
                 // if following V is nasal
-                if (i != oldphon.length - 1 && nasalvowels.indexOf(oldphon.charAt(i + 1)) >= 0) {
+                if (i < oldphon.length - 1 && nasalvowels.indexOf(oldphon.charAt(i + 1)) >= 0) {
                     nphon += "ɲ";
                 } else {
                     nphon += curr;
@@ -396,54 +398,103 @@ function PHONETICN(INPUT1) {
     return nphon;
 }
 
-// NOT FINISHED
 // Takes in a string of panãra /phonemic/[phonetic]<orthography>(author,year,page) {POR,ENG}|note|
 // and returns the string with the /phonemic/ portion filled based off of the orthography
-function PHONEMICFILL(INPUT1) {
+// and the [phonetic] stress and vowel lengthening added
+// This function is not split into two functions like the previous steps because both
+// the phonetic and phonemic forms are changed, so keeping it in one is simpler.
+function TWOPHONFILL(INPUT1) {
     return INPUT1;
-}
-
-// Takes in a string of panãra syllabified panãra phonetic form and returns the phonemic form
-function PHONETICTOPHONEMIC(INPUT1) {
-    var oldphon, nphon, vowels, nasalvowels, engmavowels, incoda;
-    oldphon = INPUT1;
-    nphon = "";
+    var unfilled, isPhon, phonetic, phonemic, postOrals, ntgems, vowels, nasalV;
+    unfilled = INPUT1;
+    isPhon = 0;
+    phonetic = "";
+    phonemic = "";
+    postOrals = GETPHONCHANGED(1);
+    ntgems = GETINITIALEPEN(1);
     vowels = GETSYLLV(1);
-    nasalvowels = GETNASALV(1);
-    engmavowels = GETENGMAV(1);
-    incoda = 0;
-    for (i = 0; i < oldphon.length - 1; i++) { // includes penultimate final sound
-        var curr = oldphon.charAt(i);
-        if (curr == "j" && incoda == 0) { // if j and in onset
-            // if j is first, or if j is preceded by a syllable break (making it C1V)
-            if (i == 0 || (i != 0 && oldphon.charAt(i - 1) == ".")) {
-                // if following V is nasal
-                if (i != oldphon.length - 1 && nasalvowels.indexOf(oldphon.charAt(i + 1)) >= 0) {
-                    nphon += "ɲ";
+    nasalV = GETNASALV(INPUT1);
+    for (i = 0; i < unfilled.length; i++) { // filling phonetic from input
+        if (unfilled.charAt(i) == "[") {
+            isPhon = 1;
+        }
+        else if (unfilled.charAt(i) == "]") {
+            break;
+        }
+        else if (isPhon == 1) {
+            filler += unfilled.charAt(i);
+        }
+    }
+    // processing for phonetic to phonemic form and phonetic stress/V length
+    // consider current two, and add to phonemic form, then move forward one
+    var phoneticNoSyll = phonetic.replace('.','');
+    var pnsLength = phoneticNoSyll.length;
+    for (i = 0; i < pnsLength; i++) {
+        var first = phoneticNoSyll.charAt(i);
+        // i epenthesis
+        if (first == "i") {
+            if (i == 0 && i < pnsLength - 2) { // checking for #_nt and #_geminates
+                var nextTwo = phoneticNoSyll.charAt(i + 1) + phoneticNoSyll.charAt(i + 2);
+                if (nextTwo in ntgems) { // if i is epenthetic
+                    continue;
                 } else {
-                    nphon += curr;
+                    phonemic += first;
+                }
+            }  else if (i > 0 && i < pnsLength - 1) { // ɾ_C
+                var prev = phoneticNoSyll.charAt(i - 1);
+                var next = phoneticNoSyll.charAt(i + 1);
+                if (prev == "ɾ" && !(next in vowels)) {
+                    continue;
+                } else {
+                    phonemic += first;
+                }
+            } else if (i > 0 && i == pnsLength - 1) { // word final
+                var prev = phoneticNoSyll.charAt(i - 1);
+                if (prev == "ɾ") { // ɾ_#
+                    continue;
+                } else if (prev == "p" || prev == "t" || prev == "s" || prev == "k") { //{p,t,s,k} -> C_#
+                    continue;
+                } else {
+                    phonemic += first;
                 }
             } else {
-                nphon += curr;
+                phonemic += first;
             }
-        } else if (vowels.indexOf(curr) >= 0) { // if V, set in coda to 1
-            incoda = 1;
-            nphon += curr;
-        } else if (curr == ".") { // if at syllable break, set in coda to 0
-            incoda = 0;
-            nphon += curr;
+        } else if (first == "ĩ") {
+            if (i > 1) { // nasal Vɾ_ (epenthesis as a nasal i) CHECK IF LONG V COUNT?
+                var prev1 = phoneticNoSyll.charAt(i - 2);
+                var prev2 = phoneticNoSyll.charAt(i - 1);
+                if (prev2 == "ɾ" && prev1 in nasalV) {
+                    continue;
+                }
+            } else {
+                phonemic += first;
+            }
+        } else if (first == "ɯ") { // ɯ epenthesis
+            if (i > 1 && i == pnsLength - 1) { // word final only ɔp_# and op_#
+                var prev1 = phoneticNoSyll.charAt(i - 2);
+                var prev2 = phoneticNoSyll.charAt(i - 1);
+                if (prev2 == "p" && (prev1 == "ɔ" || prev1 == "o")) {
+                    continue;
+                }
+            } else {
+                phonemic += first;
+            }
+        } else if (i < pnsLength - 1) { // post oralized nasals
+            var double = first + phoneticNoSyll.charAt(i + 1);
+            if (double in postOrals) {
+                phonemic += postOrals[double];
+                i += 1;
+                continue;
+            } else {
+                phonemic += first;
+            }
         } else {
-            nphon += curr;
+            phonemic += first;
         }
     }
-    if (oldphon.charAt(oldphon.length - 1) == "n") { // checking ultimate sound, if n word final
-        if (engmavowels.indexOf(oldphon.charAt(oldphon.length - 2)) >= 0) { // n is [ŋ] due to preceding V
-            nphon += "ŋ";
-        } else { // n is [ɲ] due to preceding V
-            nphon += "ɲ";
-        }
-    } else {
-        nphon += oldphon.charAt(oldphon.length - 1); // if not n, add ultimate sound
-    }
-    return nphon;
+    //ITERATE BACKWARDS NOW AND V LENGTH, STRESS, REMOVE . IN PHONEMIC OR REMOVE IT BEFORE???
+    Utilities.sleep(1000);
+    unfilled = FILLAT(unfilled, phonetic, "[", "]");
+    return FILLAT(unfilled, phonemic, "/", "/");
 }
